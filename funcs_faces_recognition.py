3f384799799094d0b36c1447d59d1f8e07a4f447
BoucheDimitri <dimitri.bouche@ensae-paristech.fr>
2018-11-17 19:28:44 +0100

    Put the functions in funcs_hfs.py and funcs_faces_recognition.py

---
+++ funcs_faces_recognition.py
@@ -0,0 +1,135 @@
+import matplotlib.pyplot as plt
+import scipy.misc as sm
+import numpy as np
+import cv2
+import os
+import sys
+import funcs_hfs
+
+
+def resize_rectangle(rect, lx, ly, xbounds, ybounds):
+	"""Reshape rectangle around its center"""
+	center = (int(rect[0] + rect[2]/2), int(rect[1] + rect[3]/2))
+	x = center[0] - int(lx/2)
+	y = center[1] - int(ly/2)
+	if x + lx > xbounds[1]:
+	    x = xbounds[1] - lx
+	elif x - lx < xbounds[0]:
+	    x = xbounds[0]
+	if y + ly > ybounds[1]:
+	    y = ybounds[1] - ly
+	elif y - ly < ybounds[0]:
+	    y = ybounds[0]
+	return x, y, lx, ly
+
+
+def load_faces(path, dataadd = "/10faces/"):
+    
+    # Parameters
+    cc = cv2.CascadeClassifier(path + 'haarcascade_frontalface_default.xml')
+    frame_size = 96
+    gamma = .95
+    var=10000
+    
+    # List folders
+    datapath = path + dataadd
+    folders = os.listdir(datapath)
+    folders.sort()
+    
+    # Infer nlabels and nimages per labels
+    nlabels = len(folders)
+    nimages_perlabel = len(os.listdir(datapath + "/" + folders[0] + "/"))
+    
+    # Loading images
+    images = np.zeros((nlabels * nimages_perlabel, frame_size ** 2))
+    labels = np.zeros(nlabels * nimages_perlabel)
+    
+    # Initialize counter variables
+    i = 0
+    j = 0
+    
+    # Loop over all files
+    for folder in os.listdir(datapath):
+        files = os.listdir(datapath + folder + "/")
+        files.sort()
+        for file in files:
+            # Read image
+            im = cv2.imread(datapath + folder + "/" + file)
+            # Detect face
+            box = cc.detectMultiScale(im)[0]
+            gray_im = cv2.cvtColor(im, cv2.COLOR_BGR2GRAY)
+            # Resize rectangle so that it fits in frame size
+            xbounds = (0, im.shape[0] - 1)
+            ybounds = (0, im.shape[1] - 1)
+            x, y, lx, ly = resize_rectangle(box, frame_size, frame_size, xbounds, ybounds)
+            # Convert to grayscale
+            gray_face = gray_im[y:y + ly, x:x + lx]
+            #resize the face and reshape it to a row vector, record labels
+            gf = gray_face.copy()
+            images[j * nlabels +  i % 10] = gf.reshape((frame_size ** 2, ))
+            labels[j * nlabels + i % 10] = j + 1
+            i += 1
+        j += 1
+    return images, labels
+
+
+def pre_process(X):
+    """Skeleton function for trying diverse pre processing operations"""
+    dim = int(np.sqrt(X.shape[1]))
+    nims = X.shape[0]
+    Xproc = [X[i, :].reshape((dim, dim)).astype(np.uint8) for i in range(0, nims)]
+    # Gaussian Blur
+    Xproc = [cv2.GaussianBlur(Xproc[i], ksize=(5, 5), sigmaX=1) for i in range(0, nims)]
+    # Histogram equalization
+#     Xproc = [cv2.equalizeHist(Xproc[i]) for i in range(0, nims)]
+    Xproc1d = np.array([Xproc[i].reshape((-1)) for i in range(0, nims)])
+    return Xproc1d
+
+
+def mask_labels_faces(Y, lper_person):
+    labels = np.unique(Y)
+    Y_masked = np.zeros(Y.shape, dtype=int)
+    for lab in labels:
+        inds = np.argwhere(Y == lab)[:, 0]
+        choice = np.random.choice(inds, lper_person, replace=False)
+        Y_masked[choice] = lab
+    return Y_masked
+
+
+def offline_face_recognition_hard(X, Y, l=4, 
+                                  laplacian_regularization=0.0001, 
+                                  var=1e3, eps=0, k=10, 
+                                  laplacian_normalization=""):
+
+    Y_masked = mask_labels_faces(Y, l)
+    labels = Y
+    rlabels = funcs_hfs.hard_hfs(X, Y_masked, laplacian_regularization, var, eps, k, laplacian_normalization)
+    
+    # Plots #
+    plt.subplot(121)
+    plt.imshow(labels.reshape((10, 10)))
+
+    plt.subplot(122)
+    plt.imshow(rlabels.reshape((10, 10)))
+    plt.title("Acc: {}".format(np.equal(rlabels, labels).mean()))
+
+    plt.show()
+
+
+def offline_face_recognition_soft(X, Y, cl=10, cu=1, l=4, 
+                                  laplacian_regularization=0.0001, 
+                                  var=1e3, eps=0, k=9, 
+                                  laplacian_normalization=""):
+
+    Y_masked = mask_labels_faces(Y, l)
+    labels = Y
+    rlabels = funcs_hfs.soft_hfs(X, Y_masked, cl, cu, laplacian_regularization, var, eps, k, laplacian_normalization)
+    # Plots #
+    plt.subplot(121)
+    plt.imshow(labels.reshape((10, 10)))
+
+    plt.subplot(122)
+    plt.imshow(rlabels.reshape((10, 10)))
+    plt.title("Acc: {}".format(np.equal(rlabels, labels).mean()))
+
+    plt.show()
---
+++ funcs_hfs.py
@@ -0,0 +1,204 @@
+import numpy as np
+import scipy
+import os
+import helper
+
+
+def one_hot_target(Y):
+    num_samples = Y.shape[0]
+    classes = np.unique(Y)
+    Yoh = np.zeros((num_samples, classes.shape[0]), dtype=int)
+    count = 0
+    for c in classes:
+        Yoh[:, count] = (Y == c).astype(int)
+        count += 1
+    return Yoh
+
+
+def mask_labels(Y, l):
+    # Draw at random index of labels
+    num_samples = Y.shape[0]
+    l_idx = np.random.choice(range(num_samples), l, replace=False)
+
+    # mask labels
+    Y_masked = np.zeros((num_samples, ), dtype = int)
+    Y_masked[l_idx] = Y[l_idx]
+    return Y_masked
+
+
+def hard_hfs(X, Y, laplacian_regularization=0.000001, var=1, eps=0, k=10, laplacian_normalization=""):
+#  a skeleton function to perform hard (constrained) HFS,
+#  needs to be completed
+#
+#  Input
+#  X:
+#      (n x m) matrix of m-dimensional samples
+#  Y:
+#      (n x 1) vector with nodes labels [1, ... , num_classes] (0 is unlabeled)
+#
+#  Output
+#  labels:
+#      class assignments for each (n) nodes
+
+    num_samples = np.size(X, 0)
+    Cl = np.unique(Y)
+    num_classes = len(Cl)-1
+
+    # Indices of labelled and unlabelled data
+    l_idx = np.argwhere(Y != 0)[:, 0]
+    u_idx = np.argwhere(Y == 0)[:, 0]
+    
+    # Build similarity graph and Laplacian
+    W = helper.build_similarity_graph(X, var, eps, k)
+    L = helper.build_laplacian(W, laplacian_normalization)
+    
+    # Extract blocks corresponding to unlabelled and labelled data
+    Luu = L[u_idx, :][:, u_idx]
+    Wul = W[u_idx, :][:, l_idx]
+    
+    # fl with one hot encoding
+    fl = one_hot_target(Y[l_idx])
+    
+    # Compute fu using regularized Laplacian
+    Q = Luu + laplacian_regularization * np.eye(u_idx.shape[0])
+    Q_inv = np.linalg.inv(Q)
+    fu = np.dot(Q_inv, np.dot(Wul, fl))
+    
+    # Infer label from computed fu using thresholding
+    fu_lab = np.argmax(fu, axis=1)
+    
+    # Consolidate labels from fu and fl
+    labels = np.zeros((num_samples, ), dtype=int)
+    # +1 because labels start at 1
+    labels[u_idx] = fu_lab + 1
+    labels[l_idx] = Y[l_idx]
+
+    return labels
+
+
+def two_moons_hfs_hard(path, l = 4, var=1, eps=0, k=10, laplacian_normalization=""):
+    # a skeleton function to perform HFS, needs to be completed
+
+    # load the data
+    in_data =scipy.io.loadmat(path+'/data/data_2moons_hfs')
+    X = in_data['X']
+    Y = in_data['Y'][:, 0]
+
+    # Draw at random index of labels
+    Y_masked = mask_labels(Y, l)
+
+    labels = hard_hfs(X, Y_masked, var=var, eps=eps, k=k, laplacian_normalization=laplacian_normalization)
+
+    helper.plot_classification(X, Y, labels,  var=var, eps=eps, k=k)
+    accuracy = np.mean(labels == np.squeeze(Y))
+    
+    return accuracy
+
+
+
+def soft_hfs(X, Y, c_l, c_u, laplacian_regularization=0.000001 ,var=1, eps=0, k=10, laplacian_normalization=""):
+    #  a skeleton function to perform soft (unconstrained) HFS,
+    #  needs to be completed
+    #
+    #  Input
+    #  X:
+    #      (n x m) matrix of m-dimensional samples
+    #  Y:
+    #      (n x 1) vector with nodes labels [1, ... , num_classes] (0 is unlabeled)
+    #  c_l,c_u:
+    #      coefficients for C matrix
+
+    #
+    #  Output
+    #  labels:
+    #      class assignments for each (n) nodes
+    num_samples = np.size(X, 0)
+    Cl = np.unique(Y)
+    num_classes = len(Cl)-1
+
+    # Indices of labelled and unlabelled data
+    l_idx = np.argwhere(Y != 0)[:, 0]
+    u_idx = np.argwhere(Y == 0)[:, 0]
+    
+    # Build C matrix
+    diagC = np.zeros((num_samples, ))
+    diagC[l_idx] = c_l
+    diagC[u_idx] = c_u
+    C = np.diag(diagC)
+    C_inv = np.diag(1 / diagC)
+    
+    # Target vector
+    y = one_hot_target(Y)[:, 1:]
+    
+    # Build similarity graph and Laplacian
+    W = helper.build_similarity_graph(X, var, eps, k)
+    L = helper.build_laplacian(W, laplacian_normalization)
+    
+    # Q matrix (regularized laplacian)
+    Q = L + laplacian_regularization * np.eye(num_samples)
+    
+    # Computation of fstar
+    D = np.dot(C_inv, Q) + np.eye(num_samples)
+    D_inv = np.linalg.inv(D)
+    fstar = np.dot(D_inv, y)
+    
+    # +1 : labels start at 1
+    labels = np.argmax(fstar, axis=1) + 1
+    
+    return labels 
+
+
+def two_moons_hfs_soft(path, cl, cu, l = 4, var=1, eps=0, k=10, laplacian_normalization=""):
+    # a skeleton function to perform HFS, needs to be completed
+    # load the data
+    in_data =scipy.io.loadmat(path+'/data/data_2moons_hfs')
+    X = in_data['X']
+    Y = in_data['Y'][:, 0]
+
+    # automatically infer number of labels from samples
+    num_samples = X.shape[0]
+    num_classes = len(np.unique(Y))
+
+    # Draw at random index of labels
+    Y_masked = mask_labels(Y, l)
+
+    labels = soft_hfs(X, Y_masked, cl, cu, var=var, eps=eps, k=k, laplacian_normalization=laplacian_normalization)
+
+    helper.plot_classification(X, Y, labels,  var=var, eps=eps, k=k)
+    accuracy = np.mean(labels == np.squeeze(Y))
+    
+    return accuracy
+
+
+def hard_vs_soft_hfs(path, cl, cu, l = 20, var=1, eps=0, k=10):
+    # a skeleton function to confront hard vs soft HFS, needs to be completed
+
+    # load the data
+    in_data =scipy.io.loadmat(path+'/data/data_2moons_hfs')
+    X = in_data['X']
+    Y = in_data['Y'][:, 0]
+
+    # automatically infer number of labels from samples
+    num_samples = X.shape[0]
+    num_classes = len(np.unique(Y))
+
+    # Draw at random index of labels
+    l_idx = np.random.choice(range(num_samples), l, replace=False)
+
+    # mask labels
+    Y_masked = np.zeros((num_samples, ), dtype = int)
+    Y_masked[l_idx] = Y[l_idx]
+    
+    
+    Y_masked[Y_masked != 0] = helper.label_noise(Y_masked[Y_masked != 0], 4)
+    
+    l_idx = np.argwhere(Y_masked != 0)
+    
+    soft_labels = soft_hfs(X, Y_masked, cl, cu, var=var, eps=eps, k=k)
+    hard_labels = hard_hfs(X, Y_masked, var=var, eps=eps, k=k)
+
+    Y_masked[Y_masked == 0] = np.squeeze(Y)[Y_masked == 0]
+
+    helper.plot_classification_comparison(X, Y, hard_labels, soft_labels, var=var, eps=eps, k=k)
+    accuracy = [np.mean(hard_labels == np.squeeze(Y)), np.mean(soft_labels == np.squeeze(Y))]
+    return accuracy
